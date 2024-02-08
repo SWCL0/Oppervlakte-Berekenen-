be5bc0ba7eb773665d5cdd419e24ef9a4d4d8ea7
SWCL0 <stan.w.c@icloud.com>
2024-02-08 10:47:18 +0100

    Update oppervlakte-berekenen.php
    Toevoeging commentaar regels

--- oppervlakte-berekenen.php
+++ oppervlakte-berekenen.php
@@ -1,12 +1,17 @@
 <?php
 
+// Opvraag lengte
 echo "Geef mij een lengte: ";
 $lengte = readline();
 
+// Opvraag breedte
 echo "Geef mij een breedte: ";
 $breedte = readline();
 
+// Berekenen totaal
 $totaal = $lengte * $breedte;
+
+// Teruggaven aan gebruiken totaal
 echo $totaal;
 
 ?>
